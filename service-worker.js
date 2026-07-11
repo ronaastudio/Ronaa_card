ef6bbef838b1b7e1679a7aa15582d0d6d974eec8
ronaastudio <m.kordian7647@gmail.com>
2026-07-11 20:35:28 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,27 @@
-self.addEventListener("install", () => {
-    console.log("Service Worker Installed");
+const CACHE_NAME = "rona-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./logo.png",
+        "./icon-192.png",
+        "./icon-512.png",
+        "./apple-touch-icon.png",
+        "./favicon.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
